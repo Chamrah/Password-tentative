8bd51da8eb985f5550edb3de19b3118dbf23a1d5
Ziad Chamrah <147912275+Chamrah@users.noreply.github.com>
2023-10-19 07:37:27 +0100

    Add files via upload

---
+++ password_tentative.py
@@ -0,0 +1,10 @@
+#program which displays message after typing the password which is limited to 3 times
+mp=input("Entrer le mot de passe correct : ")
+for i in range(1,3):
+      if(mp!="DEV@2023"):
+            mp=input("Entrer le mot de passe correct : ")
+else:
+     
+    print("Bonjour") 
+
+
